6a6fbe23b60de863776132f720f568e4cac6d395
Aidana <kamchieva.aidana@gmail.com>
2025-10-01 23:29:38 -0500

    Initial commit

---
+++ src/main/java/com/example/project/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
